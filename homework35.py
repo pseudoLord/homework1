b18f96a6e3ef78094498dbb29cc32466c0948bb3
pseudoLord <pseudolord48@gmail.com>
2025-05-21 18:47:56 +0300

    graphical interface in games

---
+++ homework35.py
@@ -0,0 +1,29 @@
+def show_emblems():
+    emblem1 = r"""
+     /\
+    /__\    EMBLEM 1
+   /\  /\   
+  /__\/__\  
+    """
+
+    emblem2 = r"""
+  _________
+ /         \ 
+|  *     *  |   EMBLEM 2
+|     ^     |   
+ \___=___/  
+    """
+
+    emblem3 = r"""
+   ______
+  / ____ \
+ | |    | |   EMBLEM 3
+ | |____| |   
+  \______/
+    """
+
+    print(emblem1)
+    print(emblem2)
+    print(emblem3)
+
+show_emblems()
